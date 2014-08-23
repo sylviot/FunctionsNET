1433e952ee07c3119f862b595e343b10b6fc0cb1
SylvioT <sylvio.tavares@hotmail.com>
2014-08-23 02:44:30 -0400

    Implementado Search Longest Common Subsequence (LCS)

---
+++ FunctionsNET/Search.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Collections.Generic;
+
+namespace FunctionsNET
+{
+    public class Search
+    {
+        public static List<string> LCS(string value, List<string> list)
+        {
+            try
+            {
+                List<dynamic> lista = new List<dynamic>();
+
+                foreach (var item in list)
+                {
+                    lista.Add(new { P = LCS(value.ToLower(), item.ToLower(), 0, 0), Word = item });
+                }
+
+                return lista.Where(x => x.P > 0)
+                            .OrderBy(x => x.Word)           //Ordenação por Nome.
+                            .OrderByDescending(x => x.P)    //Ordenação por pontuação.
+                            .Select(x => x.Word)            //Selecionar a palavra.
+                            .Cast<string>()                 //Cast para List<string>
+                            .ToList();                      //Immediate Execution
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        private static int LCS(string A, string B, int index1, int index2)
+        {
+            int max = 0;
+            if (index1 == A.Length)
+            {
+                return 0;
+            }
+            if (index2 == B.Length)
+            {
+                return 0;
+            }
+
+            for (int i = index1; i < A.Length; i++)
+            {
+                int exist = B.IndexOf(A[i], index2);
+                if (exist != -1)
+                {
+                    int temp = 1 + LCS(A, B, i + 1, exist + 1);
+                    if (max < temp)
+                    {
+                        max = temp;
+                    }
+                }
+            }
+            return max;
+        }
+    }
+}
