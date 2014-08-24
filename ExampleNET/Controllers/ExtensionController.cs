f2a54154a6ffce7b02b82596afe92005aa34c67e
SylvioT <sylvio.tavares@hotmail.com>
2014-08-24 01:31:59 -0400

    Extension - String Friendly URL

---
+++ ExampleNET/Controllers/ExtensionController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FunctionsNET.Extension;
+
+namespace ExampleNET.Controllers
+{
+    public class ExtensionController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult String()
+        {
+            string url = "URL amigável para a WEB";
+
+            ViewBag.URL = url;
+            ViewBag.URLSLUG = url.Slug();
+
+            return View();
+        }
+    }
+}
---
+++ FunctionsNET/Extension/StringExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FunctionsNET.Extension
+{
+    public static class StringExtension
+    {
+        public static string Slug(this string value)
+        {
+            var result = RemoveAccent(value).Replace("-", " ").ToLowerInvariant();
+
+            result = Regex.Replace(result, @"[^a-z0-9\s-]", string.Empty);
+            result = Regex.Replace(result, @"\s+", " ").Trim();
+
+            return Regex.Replace(result, @"\s", "-");
+        }
+
+        public static string RemoveAccent(this string value)
+        {
+            var bytes = Encoding.GetEncoding("Cyrillic").GetBytes(value);
+            return Encoding.ASCII.GetString(bytes);
+        }
+    }
+}
